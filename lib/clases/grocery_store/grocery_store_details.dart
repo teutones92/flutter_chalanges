123e2fe48d06952c46ec91d28d23a5edf914a55e
teutones92 <teutones92@gmail.com>
2021-09-16 11:57:36 -0400

    convirtiendo a tema oscuro trabajando en details page

--- lib/clases/grocery_store/grocery_store_details.dart
+++ lib/clases/grocery_store/grocery_store_details.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chalanges/clases/grocery_store/grocery_store_page.dart';
 import 'package:flutter_chalanges/clases/grocery_store/models/grocery_products.dart';
 
 class GroceryStoreDetails extends StatelessWidget {
@@ -10,15 +11,56 @@ class GroceryStoreDetails extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(),
-      body: Center(
-        child: Hero(
-          tag: 'list_${product.name}',
-          child: Image.asset(
-            product.image,
-            fit: BoxFit.cover,
-          ),
+      appBar: AppBar(
+        elevation: 0,
+        leading: BackButton(
+          color: Colors.black,
         ),
+        backgroundColor: Colors.white,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Column(
+              children: [
+                Center(
+                  child: Hero(
+                    tag: 'list_${product.name}',
+                    child: Image.asset(
+                      product.image,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.favorite_border),
+                color: Colors.black,
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Agregar al Carrito',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20),
+                  ),
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(20),),
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.yellow)),
+                ),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
